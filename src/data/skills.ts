5d5b1048bb29465127c6a882453a569ac9d8afe0
moreganooooo <escott.morgan@gmail.com>
2025-06-08 00:01:13 -0400

    Create skills.ts

---
+++ src/data/skills.ts
@@ -0,0 +1,25 @@
+import { Palette, GitCompareArrows, Send, LayoutDashboard } from "lucide-react";
+import type { Skill } from "@/types";
+
+export const skills: Skill[] = [
+  {
+    icon: GitCompareArrows,
+    title: "Lifecycle Marketing",
+    description: "Building customer journeys that convert and retain",
+  },
+  {
+    icon: Send,
+    title: "Email Marketing",
+    description: "Messaging that resonates with every audience",
+  },
+  {
+    icon: LayoutDashboard,
+    title: "CRM Optimization",
+    description: "Turning data into actionable insights and strategies",
+  },
+  {
+    icon: Palette,
+    title: "Copywriting  & Design",
+    description: "Crafting compelling narratives and visuals",
+  },
+];
