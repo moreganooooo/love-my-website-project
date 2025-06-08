926118974c776103069e0585b3f35ce205885861
moreganooooo <escott.morgan@gmail.com>
2025-06-08 00:07:47 -0400

    Create index.ts

---
+++ types/index.ts
@@ -0,0 +1,10 @@
+import { LucideIcon } from "lucide-react";
+
+/**
+ * Describes a skill item used in the About section
+ */
+export type Skill = {
+  icon: LucideIcon;
+  title: string;
+  description: string;
+};
