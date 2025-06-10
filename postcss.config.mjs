15293141d8b3e9a47adf50addfd4c4aec028f16a
moreganooooo <escott.morgan@gmail.com>
2025-06-10 10:42:01 -0400

    {commit message}

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,6 +2,9 @@
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
-export default {
-  plugins: [tailwindcss, autoprefixer],
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
