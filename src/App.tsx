2be9a3dfeb729b5fbd4a056f550ede9c96a58886
moreganooooo <escott.morgan@gmail.com>
2025-06-10 14:23:21 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from '@/components/ui/toaster';
 import { Toaster as Sonner } from '@/components/ui/sonner';
 import { TooltipProvider } from '@/components/ui/tooltip';
@@ -20,7 +19,8 @@ const App = () => (
         <Navigation />
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/sample-project" element={<SampleProject />} />
+          {/* <-- Updated path below! */}
+          <Route path="/projects/sample-project" element={<SampleProject />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
