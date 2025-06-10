08a314fa868dc9ab190171611115ef27790116b2
moreganooooo <escott.morgan@gmail.com>
2025-06-10 14:31:35 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navigation from '@/components/shared/Navigation';
 import Index from './pages/Index';
-import SampleProject from './pages/sample-project';
+import SampleProject from './pages/SampleProject';
 import NotFound from './pages/NotFound';
 
 const queryClient = new QueryClient();
